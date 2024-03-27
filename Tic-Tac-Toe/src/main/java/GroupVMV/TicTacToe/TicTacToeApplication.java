6b8d5edfc828a90c9b56b290d34bcf17737a6478
Vasil <vasilbarakov1@gmail.com>
2024-03-27 11:12:54 +0200

    Add files via upload

---
+++ Tic-Tac-Toe/src/main/java/GroupVMV/TicTacToe/TicTacToeApplication.java
@@ -0,0 +1,18 @@
+package GroupVMV.TicTacToe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+
+
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+
+}
