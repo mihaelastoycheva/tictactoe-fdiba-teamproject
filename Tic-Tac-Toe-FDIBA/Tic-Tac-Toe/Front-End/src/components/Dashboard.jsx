3a174fc96f1eb3ce5ea02b524cb960d13c4eb018
mihaelastoycheva <msdesign.business@gmail.com>
2024-04-26 17:15:33 +0300

    added get request with axios with example data

--- Tic-Tac-Toe-FDIBA/Tic-Tac-Toe/Front-End/src/components/Dashboard.jsx
+++ Tic-Tac-Toe-FDIBA/Tic-Tac-Toe/Front-End/src/components/Dashboard.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from 'axios';
+import { useEffect } from "react";
 
-export function Dashboard ( ) {
+export function Dashboard() {
+  const[data, setData] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log("Getting from ::: test ", res.data)
+        setData(res.data)
+      }).catch(err => console.log(err))
+  }, [])
+
+  const arr = data.map((data, index) => {
+    return(
+      <tr>
+        <td>{data.id}</td>
+        <td>{data.title}</td>
+        <td>{data.body}</td>
+
+      </tr>
+    )
+  })
+
   return (
     <div className="table-dashboard">
       <h2>Dashboard</h2>
       <table>
         <thead>
           <tr>
-            <th>User</th>
-            <th>Wins</th>
+            <th>User/ID</th>
+            <th>Wins/Title</th>
+            <th>Body</th>
           </tr>
+         
         </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-          </tr>
+        {arr}
           {/*{userData.map((user, index) => (
             <tr key={index}>
               <td>{user.name}</td>
               <td>{user.wins}</td>
             </tr>
           ))} */}
-        </tbody>
+        
       </table>
     </div>
   );
